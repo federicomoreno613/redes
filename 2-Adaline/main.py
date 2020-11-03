ae97cf72298fe30df83d848509d60b25818dc873
jduqer <jduqer@gmail.com>
2020-11-03 17:11:42 -0300

    update main 2

--- 2-Adaline/main.py
+++ 2-Adaline/main.py
@@ -4,27 +4,37 @@ Created on Fri Aug 18 10:35:35 2017
 
 @author: Waldo Hasperué
 """
-
+#%%
 from adaline  import train
 from ImagenPuntosMPL import AbrirImagen
 
-root_path = "D:\\Facultad\\Catedras\\UBA\\Teorías\\02 - Perceptrón\\Python\\"
+#%%
+root_path = "D:\\DataMining\\Redes Neuronales\\redes\\2-Adaline\\"
 
 file_path = root_path  + "Imagen 1.bmp"
 
+#%%
 X = AbrirImagen(file_path)
-column_count = 3;
+column_count = 3
 
 T = X[:, column_count - 1]
 P = X[:, 0:(column_count - 1)]
 
-alfa = 0.1
+alfa = 0.5
 max_ite = 50
 cota = 0.00001
 
 funcion = 'tansig'
 T = T * 2 - 1
 
+#%%
+xmax = P.max()
+xmin = P.min()
+P = (P - xmin)/(xmax - xmin)
+
+#%%
 (W, b, ite, error_prom) = train(P, T, alfa, max_ite, cota, funcion, True)
 
 
+
+# %%
